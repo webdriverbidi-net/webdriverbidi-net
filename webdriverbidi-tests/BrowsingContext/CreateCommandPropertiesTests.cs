096e122c3ace4a32f1516904189e2c64f1553f8a
Jim Evans <james.h.evans.jr@gmail.com>
2022-10-12 18:40:38 -0400

    fix: Add referenceContext to browsingContext.create command

--- webdriverbidi-tests/BrowsingContext/CreateCommandPropertiesTests.cs
+++ webdriverbidi-tests/BrowsingContext/CreateCommandPropertiesTests.cs
@@ -18,7 +18,7 @@ public class CreateCommandPropertiesTests
         Assert.That(serialized["type"]!.Value<string>(), Is.EqualTo("tab"));
     }
 
-   [Test]
+    [Test]
     public void TestCanSerializePropertiesForWindow()
     {
         var properties = new CreateCommandSettings(BrowsingContextCreateType.Window);
@@ -28,5 +28,21 @@ public class CreateCommandPropertiesTests
         Assert.That(serialized.ContainsKey("type"));
         Assert.That(serialized["type"]!.Type, Is.EqualTo(JTokenType.String));
         Assert.That(serialized["type"]!.Value<string>(), Is.EqualTo("window"));
+    }
+
+    [Test]
+    public void TestCanSerializePropertiesWithReferenceContext()
+    {
+        var properties = new CreateCommandSettings(BrowsingContextCreateType.Tab);
+        properties.ReferenceContextId = "myReferenceContext";
+        string json = JsonConvert.SerializeObject(properties);
+        JObject serialized = JObject.Parse(json);
+        Assert.That(serialized.Count, Is.EqualTo(2));
+        Assert.That(serialized.ContainsKey("type"));
+        Assert.That(serialized["type"]!.Type, Is.EqualTo(JTokenType.String));
+        Assert.That(serialized["type"]!.Value<string>(), Is.EqualTo("tab"));
+        Assert.That(serialized.ContainsKey("referenceContext"));
+        Assert.That(serialized["referenceContext"]!.Type, Is.EqualTo(JTokenType.String));
+        Assert.That(serialized["referenceContext"]!.Value<string>(), Is.EqualTo("myReferenceContext"));
     }
 }
