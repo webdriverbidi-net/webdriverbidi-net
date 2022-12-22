4348a997f58d26356f403010d88bdc9ce9133cbd
Jim Evans <james.h.evans.jr@gmail.com>
2022-12-22 13:07:12 -0500

    fix: Make UnsubscribeCommandSettings subclass of SubscribeCommandSettings

--- webdriverbidi/Session/UnsubscribeCommandSettings.cs
+++ webdriverbidi/Session/UnsubscribeCommandSettings.cs
@@ -1,39 +1,13 @@
 namespace WebDriverBidi.Session;
 
-using System;
 using Newtonsoft.Json;
 
 [JsonObject(MemberSerialization.OptIn)]
-public class UnsubscribeCommandSettings : CommandSettings
+public class UnsubscribeCommandSettings : SubscribeCommandSettings
 {
-    private List<string> eventList = new List<string>();
-
-    private List<string> contextList = new List<string>();
-
     public UnsubscribeCommandSettings()
     {
     }
 
     public override string MethodName => "session.unsubscribe";
-
-    public override Type ResultType => typeof(EmptyResult);
-
-    [JsonProperty("events")]
-    public List<string> Events => this.eventList;
-
-    public List<string> Contexts => this.contextList;
-
-    [JsonProperty("contexts", NullValueHandling = NullValueHandling.Ignore)]
-    internal List<string>? SeralizableContexts
-    {
-        get
-        {
-            if (this.contextList.Count == 0)
-            {
-                return null;
-            }
-
-            return this.contextList;
-        }
-    }
 }
