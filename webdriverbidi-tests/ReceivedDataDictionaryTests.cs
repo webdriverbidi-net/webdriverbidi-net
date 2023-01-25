001cf37fc360ba1d5331233bdb9c2ec58f9e6a9c
Jim Evans <james.h.evans.jr@gmail.com>
2023-01-24 19:13:06 -0500

    fix: Add unit tests for received data list and dictionary

---
+++ webdriverbidi-tests/ReceivedDataDictionaryTests.cs
@@ -0,0 +1,102 @@
+namespace WebDriverBidi;
+
+[TestFixture]
+public class ReceivedDataDictionaryTests
+{
+    [Test]
+    public void TestCanWrapDictionary()
+    {
+        Dictionary<string, object?> dictionary = new()
+        {
+            { "string", "myString" },
+            { "long", 23 },
+            { "bool", true },
+            { "null", null },
+            { "list", new List<object?>()
+                {
+                    "list string",
+                    45,
+                    true,
+                    null,
+                    new List<object?>() { "list list string", null, 67 },
+                    new Dictionary<string, object?>() { { "list dictionary string", "string" } }
+                }
+            },
+            { "dictionary", new Dictionary<string, object?>()
+                {
+                    { "dictionary dictionary string", "string" }
+                }
+            }
+        };
+
+        ReceivedDataDictionary received = new(dictionary);
+        IList<object?>? wrappedList = received["list"] as IList<object?>;
+        IDictionary<string, object?>? wrappedDict = received["dictionary"] as IDictionary<string, object?>;
+        Assert.Multiple(() =>
+        {
+            Assert.That(received["string"], Is.EqualTo("myString"));
+            Assert.That(received["long"], Is.EqualTo(23));
+            Assert.That(received["bool"], Is.EqualTo(true));
+            Assert.That(received["null"], Is.Null);
+            Assert.That(wrappedList, Is.Not.Null);
+            Assert.That(wrappedList!, Is.InstanceOf<ReceivedDataList>());
+            Assert.That(() => wrappedList!.Add("foo"), Throws.InstanceOf<NotSupportedException>());
+            Assert.That(wrappedDict, Is.Not.Null);
+            Assert.That(wrappedDict, Is.InstanceOf<ReceivedDataDictionary>());
+            Assert.That(() => wrappedDict!["newKey"] = "newValue", Throws.InstanceOf<NotSupportedException>());
+            Assert.That(() => wrappedDict!["string"] = "newValue", Throws.InstanceOf<NotSupportedException>());
+        });
+    }
+
+    [Test]
+    public void TestCanUnwrapDictionaryToCopy()
+    {
+        Dictionary<string, object?> dictionary = new()
+        {
+            { "string", "myString" },
+            { "long", 23 },
+            { "bool", true },
+            { "null", null },
+            { "list", new List<object?>()
+                {
+                    "list string",
+                    45,
+                    true,
+                    null,
+                    new List<object?>() { "list list string", null, 67 },
+                    new Dictionary<string, object?>() { { "list dictionary string", "string" } }
+                }
+            },
+            { "dictionary", new Dictionary<string, object?>()
+                {
+                    { "dictionary dictionary string", "string" }
+                }
+            }
+        };
+
+        ReceivedDataDictionary received = new(dictionary);
+        IDictionary<string, object?> unwrapped = received.ToWritableCopy();
+        IList<object?>? unwrappedList = unwrapped["list"] as IList<object?>;
+        IDictionary<string, object?>? unwrappedDict = unwrapped["dictionary"] as IDictionary<string, object?>;
+        Assert.Multiple(() =>
+        {
+            Assert.That(unwrapped["string"], Is.EqualTo("myString"));
+            Assert.That(unwrapped["long"], Is.EqualTo(23));
+            Assert.That(unwrapped["bool"], Is.EqualTo(true));
+            Assert.That(unwrapped["null"], Is.Null);
+            Assert.That(unwrappedList, Is.Not.Null);
+            Assert.That(unwrappedList!, Is.InstanceOf<List<object?>>());
+            Assert.That(unwrappedDict, Is.Not.Null);
+            Assert.That(unwrappedDict, Is.InstanceOf<Dictionary<string, object?>>());
+        });
+
+        unwrappedList!.Add("foo");
+        unwrappedDict!["bar"] = "baz";
+    }
+
+    [Test]
+    public void TestEmptyDictionary()
+    {
+        Assert.That(ReceivedDataDictionary.EmptyDictionary, Is.Empty);
+    }
+}
---
+++ webdriverbidi-tests/ReceivedDataListTests.cs
@@ -0,0 +1,98 @@
+namespace WebDriverBidi;
+
+[TestFixture]
+public class ReceivedDataListTests
+{
+    [Test]
+    public void TestCanWrapList()
+    {
+        List<object?> list = new()
+        {
+            "myString",
+            23,
+            true,
+            null,
+            new List<object?>()
+            {
+                "list string",
+                45,
+                true,
+                null,
+                new List<object?>() { "list list string", null, 67 },
+                new Dictionary<string, object?>() { { "list dictionary string", "string" } }
+            },
+            new Dictionary<string, object?>()
+            {
+                { "dictionary dictionary string", "string" }
+            }
+        };
+
+        ReceivedDataList received = new(list);
+        IList<object?>? wrappedList = received[4] as IList<object?>;
+        IDictionary<string, object?>? wrappedDict = received[5] as IDictionary<string, object?>;
+        Assert.Multiple(() =>
+        {
+            Assert.That(received[0], Is.EqualTo("myString"));
+            Assert.That(received[1], Is.EqualTo(23));
+            Assert.That(received[2], Is.EqualTo(true));
+            Assert.That(received[3], Is.Null);
+            Assert.That(wrappedList, Is.Not.Null);
+            Assert.That(wrappedList!, Is.InstanceOf<ReceivedDataList>());
+            Assert.That(() => wrappedList!.Add("foo"), Throws.InstanceOf<NotSupportedException>());
+            Assert.That(wrappedDict, Is.Not.Null);
+            Assert.That(wrappedDict, Is.InstanceOf<ReceivedDataDictionary>());
+            Assert.That(() => wrappedDict!["newKey"] = "newValue", Throws.InstanceOf<NotSupportedException>());
+            Assert.That(() => wrappedDict!["string"] = "newValue", Throws.InstanceOf<NotSupportedException>());
+        });
+    }
+
+    [Test]
+    public void TestCanUnwrapListToCopy()
+    {
+        List<object?> list = new()
+        {
+            "myString",
+            23,
+            true,
+            null,
+            new List<object?>()
+            {
+                "list string",
+                45,
+                true,
+                null,
+                new List<object?>() { "list list string", null, 67 },
+                new Dictionary<string, object?>() { { "list dictionary string", "string" } }
+            },
+            new Dictionary<string, object?>()
+            {
+                { "dictionary dictionary string", "string" }
+            }
+        };
+
+        ReceivedDataList received = new(list);
+        List<object?> unwrapped = received.ToWritableCopy();
+        IList<object?>? unwrappedList = unwrapped[4] as IList<object?>;
+        IDictionary<string, object?>? unwrappedDict = unwrapped[5] as IDictionary<string, object?>;
+        Assert.Multiple(() =>
+        {
+            Assert.That(unwrapped[0], Is.EqualTo("myString"));
+            Assert.That(unwrapped[1], Is.EqualTo(23));
+            Assert.That(unwrapped[2], Is.EqualTo(true));
+            Assert.That(unwrapped[3], Is.Null);
+            Assert.That(unwrappedList, Is.Not.Null);
+            Assert.That(unwrappedList!, Is.InstanceOf<List<object?>>());
+            Assert.That(unwrappedDict, Is.Not.Null);
+            Assert.That(unwrappedDict, Is.InstanceOf<Dictionary<string, object?>>());
+        });
+
+        unwrappedList!.Add("foo");
+        unwrappedDict!["bar"] = "baz";
+    }
+
+    [Test]
+    public void TestEmptyDictionary()
+    {
+        Assert.That(ReceivedDataList.EmptyList, Is.Empty);
+    }
+}
