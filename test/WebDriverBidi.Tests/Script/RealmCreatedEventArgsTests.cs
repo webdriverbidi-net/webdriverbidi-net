24007b21aebb38fb761d31e04e7fafb665caea7d
Jim Evans <james.h.evans.jr@gmail.com>
2023-06-30 17:45:54 -0400

    fix: Add specific unit tests for RealmCreatedEventArgs

---
+++ test/WebDriverBidi.Tests/Script/RealmCreatedEventArgsTests.cs
@@ -0,0 +1,37 @@
+namespace WebDriverBidi.Script;
+
+using Newtonsoft.Json;
+
+[TestFixture]
+public class RealmCreatedEventArgsTests
+{
+    [Test]
+    public void TestCanCreateWithWindowRealmInfo()
+    {
+        string json = @"{ ""realm"": ""myRealm"", ""origin"": ""myOrigin"", ""type"": ""window"", ""context"": ""myContext"" }";
+        RealmInfo? info = JsonConvert.DeserializeObject<RealmInfo>(json);
+        RealmCreatedEventArgs eventArgs = new(info!);
+        Assert.Multiple(() =>
+        {
+            Assert.That(eventArgs.RealmId, Is.EqualTo("myRealm"));
+            Assert.That(eventArgs.Origin, Is.EqualTo("myOrigin"));
+            Assert.That(eventArgs.Type, Is.EqualTo(RealmType.Window));
+            Assert.That(eventArgs.BrowsingContext, Is.EqualTo("myContext"));
+        });
+    }
+
+    [Test]
+    public void TestCanCreateWithNonWindowRealmInfo()
+    {
+        string json = @"{ ""realm"": ""myRealm"", ""origin"": ""myOrigin"", ""type"": ""worker"" }";
+        RealmInfo? info = JsonConvert.DeserializeObject<RealmInfo>(json);
+        RealmCreatedEventArgs eventArgs = new(info!);
+        Assert.Multiple(() =>
+        {
+            Assert.That(eventArgs.RealmId, Is.EqualTo("myRealm"));
+            Assert.That(eventArgs.Origin, Is.EqualTo("myOrigin"));
+            Assert.That(eventArgs.Type, Is.EqualTo(RealmType.Worker));
+            Assert.That(eventArgs.BrowsingContext, Is.Null);
+        });
+    }
+}
