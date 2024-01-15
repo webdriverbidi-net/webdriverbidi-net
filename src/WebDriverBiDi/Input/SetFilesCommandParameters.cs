aa07018d003d099562299396c82948337dd246e2
Jim Evans <james.h.evans.jr@gmail.com>
2024-01-15 15:01:14 -0500

    feat: Add input.setFiles command

---
+++ src/WebDriverBiDi/Input/SetFilesCommandParameters.cs
@@ -0,0 +1,54 @@
+// <copyright file="SetFilesCommandParameters.cs" company="WebDriverBiDi.NET Committers">
+// Copyright (c) WebDriverBiDi.NET Committers. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace WebDriverBiDi.Input;
+
+using System.Text.Json.Serialization;
+using WebDriverBiDi.Script;
+
+/// <summary>
+/// Provides parameters for the input.setFiles command.
+/// </summary>
+public class SetFilesCommandParameters : CommandParameters<EmptyResult>
+{
+    private readonly List<string> files = new();
+    private string browsingContextId;
+    private SharedReference element;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SetFilesCommandParameters"/> class.
+    /// </summary>
+    /// <param name="browsingContextId">The browsing context ID containing the element for which to set the files.</param>
+    /// <param name="element">The element for which to set the file list. Must be of type {input type="file"}.</param>
+    public SetFilesCommandParameters(string browsingContextId, SharedReference element)
+    {
+        this.browsingContextId = browsingContextId;
+        this.element = element;
+    }
+
+    /// <summary>
+    /// Gets the method name of the command.
+    /// </summary>
+    [JsonIgnore]
+    public override string MethodName => "input.setFiles";
+
+    /// <summary>
+    /// Gets or sets the browsing context ID containing the element for which to set the files.
+    /// </summary>
+    [JsonPropertyName("context")]
+    public string BrowsingContextId { get => this.browsingContextId; set => this.browsingContextId = value; }
+
+    /// <summary>
+    /// Gets or sets the element for which to set the file list. Note that the element must be of type {input type="file"}.
+    /// </summary>
+    [JsonPropertyName("element")]
+    public SharedReference Element { get => this.element; set => this.element = value; }
+
+    /// <summary>
+    /// Gets the list of files to be set on the element.
+    /// </summary>
+    [JsonPropertyName("files")]
+    public List<string> Files => this.files;
+}
---
+++ test/WebDriverBiDi.Tests/Input/SetFileCommandParametersTests.cs
@@ -0,0 +1,79 @@
+namespace WebDriverBiDi.Input;
+
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+using WebDriverBiDi.Script;
+
+[TestFixture]
+public class SetFilesCommandParametersTests
+{
+   [Test]
+    public void TestCommandName()
+    {
+        SharedReference element = new("mySharedId");
+        SetFilesCommandParameters properties = new("myContextId", element);
+        Assert.That(properties.MethodName, Is.EqualTo("input.setFiles"));
+    }
+
+    [Test]
+    public void TestCanSerializeParameters()
+    {
+        SharedReference element = new("mySharedId");
+        SetFilesCommandParameters properties = new("myContextId", element);
+        string json = JsonSerializer.Serialize(properties);
+        JObject serialized = JObject.Parse(json);
+        Assert.That(serialized, Has.Count.EqualTo(3));
+        Assert.Multiple(() =>
+        {
+            Assert.That(serialized, Contains.Key("context"));
+            Assert.That(serialized["context"]!.Type, Is.EqualTo(JTokenType.String));
+            Assert.That(serialized["context"]!.Value<string>(), Is.EqualTo("myContextId"));
+            Assert.That(serialized, Contains.Key("element"));
+            Assert.That(serialized["element"]!.Type, Is.EqualTo(JTokenType.Object));
+            Assert.That(serialized["element"]!.Value<JObject>(), Is.Not.Null);
+            JObject elementObject = serialized["element"]!.Value<JObject>()!;
+            Assert.That(elementObject, Has.Count.EqualTo(1));
+            Assert.That(elementObject, Contains.Key("sharedId"));
+            Assert.That(elementObject["sharedId"]!.Type, Is.EqualTo(JTokenType.String));
+            Assert.That(elementObject["sharedId"]!.Value<string>(), Is.EqualTo("mySharedId"));
+            Assert.That(serialized, Contains.Key("files"));
+            Assert.That(serialized["files"]!.Type, Is.EqualTo(JTokenType.Array));
+            Assert.That(serialized["files"]!, Is.Empty);
+        });
+    }
+
+    [Test]
+    public void TestCanSerializeParametersWithFileList()
+    {
+        SharedReference element = new("mySharedId");
+        SetFilesCommandParameters properties = new("myContextId", element);
+        properties.Files.Add("path/to/file1.txt");
+        properties.Files.Add("path/to/file2.txt");
+        string json = JsonSerializer.Serialize(properties);
+        JObject serialized = JObject.Parse(json);
+        Assert.That(serialized, Has.Count.EqualTo(3));
+        Assert.Multiple(() =>
+        {
+            Assert.That(serialized, Contains.Key("context"));
+            Assert.That(serialized["context"]!.Type, Is.EqualTo(JTokenType.String));
+            Assert.That(serialized["context"]!.Value<string>(), Is.EqualTo("myContextId"));
+            Assert.That(serialized, Contains.Key("element"));
+            Assert.That(serialized["element"]!.Type, Is.EqualTo(JTokenType.Object));
+            Assert.That(serialized["element"]!.Value<JObject>(), Is.Not.Null);
+            JObject elementObject = serialized["element"]!.Value<JObject>()!;
+            Assert.That(elementObject, Has.Count.EqualTo(1));
+            Assert.That(elementObject, Contains.Key("sharedId"));
+            Assert.That(elementObject["sharedId"]!.Type, Is.EqualTo(JTokenType.String));
+            Assert.That(elementObject["sharedId"]!.Value<string>(), Is.EqualTo("mySharedId"));
+            Assert.That(serialized, Contains.Key("files"));
+            Assert.That(serialized["files"]!.Type, Is.EqualTo(JTokenType.Array));
+            Assert.That(serialized["files"]!.Value<JArray>(), Is.Not.Null);
+            JArray filesArray = serialized["files"]!.Value<JArray>()!;
+            Assert.That(filesArray, Has.Count.EqualTo(2));
+            Assert.That(filesArray[0].Type, Is.EqualTo(JTokenType.String));
+            Assert.That(filesArray[0].Value<string>(), Is.EqualTo("path/to/file1.txt"));
+            Assert.That(filesArray[1].Type, Is.EqualTo(JTokenType.String));
+            Assert.That(filesArray[1].Value<string>(), Is.EqualTo("path/to/file2.txt"));
+        });
+    }
+}
