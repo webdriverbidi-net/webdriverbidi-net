dba1ce0ca100a9bb54a04c431e8e7c40bfd00dab
Jim Evans <james.h.evans.jr@gmail.com>
2026-06-09 16:39:38 -0400

    fix(client): add Async suffix to SendKeysToElement extension method

--- src/WebDriverBiDi.Client/Extensions/InputModuleExtensions.cs
+++ src/WebDriverBiDi.Client/Extensions/InputModuleExtensions.cs
@@ -41,7 +41,7 @@ public static class InputModuleExtensions
     /// <param name="timeoutOverride">The timeout override to use for the command. If omitted, the value of <see cref="BiDiDriver.DefaultCommandTimeout"/> is used.</param>
     /// <param name="cancellationToken">A cancellation token used to propagate notification that the operation should be canceled. Omitting this argument is the equivalent of using <see cref="CancellationToken.None"/>.</param>
     /// <returns>An Task representing the asynchronous operation.</returns>
-    public static async Task SendKeysToElement(this InputModule module, string browsingContextId, SharedReference elementReference, string keysToSend, TimeSpan? timeoutOverride = null, CancellationToken cancellationToken = default)
+    public static async Task SendKeysToElementAsync(this InputModule module, string browsingContextId, SharedReference elementReference, string keysToSend, TimeSpan? timeoutOverride = null, CancellationToken cancellationToken = default)
     {
         InputBuilder inputBuilder = new();
         inputBuilder.AddClickOnElementAction(elementReference);
