e75e2cd6ed1c9c236d907bec9b7c4781b3c60e12
jimevans <james.h.evans.jr@gmail.com>
2026-09-14 12:58:13 -0400

    feat: add cast methods to Script.EvaluateResult

--- src/WebDriverBiDi/Script/EvaluateResult.cs
+++ src/WebDriverBiDi/Script/EvaluateResult.cs
@@ -5,6 +5,7 @@
 
 namespace WebDriverBiDi.Script;
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 using WebDriverBiDi.JsonConverters;
 
@@ -39,4 +40,42 @@ public record EvaluateResult : CommandResult
     [JsonRequired]
     [JsonInclude]
     public string RealmId { get; internal set; } = string.Empty;
+
+    /// <summary>
+    /// Casts this <see cref="EvaluateResult"/> to a type-specific evaluation result, throwing if it is not of that
+    /// type. Use <see cref="TryAs{T}"/> to test without throwing.
+    /// </summary>
+    /// <typeparam name="T">The specific type of EvaluateResult to return.</typeparam>
+    /// <returns>This instance cast to the specified correct type.</returns>
+    /// <exception cref="WebDriverBiDiException">Thrown if this EvaluateResult is not the specified type.</exception>
+    public T As<T>()
+        where T : EvaluateResult
+    {
+        if (this is T converted)
+        {
+            return converted;
+        }
+
+        throw new WebDriverBiDiException($"EvaluateResult of type '{this.ResultType}' cannot be cast to type '{typeof(T).Name}'");
+    }
+
+    /// <summary>
+    /// Attempts to cast this <see cref="EvaluateResult"/> to a type-specific evaluation result, returning
+    /// <see langword="false"/> rather than throwing when it is not of that type.
+    /// </summary>
+    /// <typeparam name="T">The specific type of EvaluateResult to return.</typeparam>
+    /// <param name="result">When this method returns, contains the cast value or null if the cast failed.</param>
+    /// <returns><see langword="true"/> if the cast was successful; otherwise, <see langword="false"/>.</returns>
+    public bool TryAs<T>([NotNullWhen(true)] out T? result)
+        where T : EvaluateResult
+    {
+        if (this is T converted)
+        {
+            result = converted;
+            return true;
+        }
+
+        result = null;
+        return false;
+    }
 }
